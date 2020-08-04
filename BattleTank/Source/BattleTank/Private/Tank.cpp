6e4d3d41742b4248d1e61d1ddddd039dff7e4f06
ktonegawa <kazuma.tonegawa@gmail.com>
2020-08-04 14:28:30 -0700

    set up barrel aim stuff

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -9,6 +9,13 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+    // no need to protect ponter as added construction
+    TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
+}
+
+void ATank::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
+{
+    TankAimingComponent->SetBarrelReference(BarrelToSet);
 }
 
 // Called when the game starts or when spawned
@@ -34,6 +41,5 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void ATank::AimAt(FVector HitLocation)
 {
-    auto OurTankName = GetName();
-    UE_LOG(LogTemp, Warning, TEXT("%s aiming at direction: %s"), *(OurTankName), *(HitLocation.ToString()));
+    TankAimingComponent->AimAt(HitLocation);
 }
