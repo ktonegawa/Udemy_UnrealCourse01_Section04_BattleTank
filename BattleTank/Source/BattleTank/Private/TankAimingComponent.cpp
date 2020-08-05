f3885ee668095398f0cc6a2096e179af5d26580a
ktonegawa <kazuma.tonegawa@gmail.com>
2020-08-04 20:15:54 -0700

    completed lecture video 149

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -42,8 +42,14 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
         auto AimDirection = OutLaunchVelocity.GetSafeNormal();
         // UE_LOG(LogTemp, Warning, TEXT("Aiming at : %s"), *(AimDirection.ToString()));
         MoveBarrelTowards(AimDirection);
+        auto Time = GetWorld()->GetTimeSeconds();
+        UE_LOG(LogTemp, Warning, TEXT("%f: Aim solution found"), Time);
     }
-    
+    else
+    {
+        auto Time = GetWorld()->GetTimeSeconds();
+        UE_LOG(LogTemp, Warning, TEXT("%f: No aim solution found"), Time);
+    }
     
     
 }
