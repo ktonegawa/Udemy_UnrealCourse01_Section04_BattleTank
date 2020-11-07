ef59552409a112c7beb5abd013982287148c243c
ktonegawa <kazuma.tonegawa@gmail.com>
2020-11-06 18:23:52 -0800

    lecture 170

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,9 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
+
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet) {
+    LeftTrack = LeftTrackToSet;
+    RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw) {
     UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+
+    if (!LeftTrack || !RightTrack) { return; }
+    LeftTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(Throw);
+    // TODO prevent double-speed due to dual control use
 }
 
 
+void UTankMovementComponent::IntendTurnRight(float Throw) {
+    UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+
+    LeftTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(-Throw);
+    // TODO prevent double-speed due to dual control use
+}
