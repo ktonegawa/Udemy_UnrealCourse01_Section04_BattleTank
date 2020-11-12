3b07cb0cf3f111ad3d894e8e982b3a22f0966a61
ktonegawa <kazuma.tonegawa@gmail.com>
2020-11-12 15:59:57 -0800

    lecture 206

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -14,10 +14,15 @@ class BATTLETANK_API ATank : public APawn
 public:
     // called by engine when damage is dealt
     virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
-	// Sets default values for this pawn's properties
-	ATank();
+	
+    // returns current health as a percentage of starting health between 0 and 1
+    UFUNCTION(BlueprintPure, Category = "Health")
+    float GetHealthPercent() const;
 
 private:
+    // Sets default values for this pawn's properties
+    ATank();
+
     UPROPERTY(EditDefaultsOnly, Category = "Setup")
     int32 StartingHealth = 100;
     UPROPERTY(VisibleAnywhere, Category = "Health")
